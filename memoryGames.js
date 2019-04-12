a68d41b52b8895d6870dc9463ff3da7bbf56fb91
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-12 12:38:21 -0400

    add id incrementing logic

--- memoryGames.js
+++ memoryGames.js
@@ -1,14 +1,25 @@
 const Promise = require('promise');
 
 let gameStore = [ ];
+let lastId = 0;
+
+const generateId = () => {
+  lastId = lastId + 1;
+  return lastId;
+};
 
 module.exports = {
+
   insert: newGame => {
+    newGame.id = generateId();
     gameStore.push(newGame);
     return Promise.resolve(newGame);
   },
+
   get: () => Promise.resolve(gameStore),
+
   truncate: () => {
+    lastId = 0;
     gameStore = [];
     return Promise.resolve();
   }
